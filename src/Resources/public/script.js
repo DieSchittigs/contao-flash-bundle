8252db498eaee454230c1e0be24e735290b91df5
Malte Heyenga <malte.heyenga@googlemail.com>
2019-07-23 10:33:11 +0200

    fix #1

--- src/Resources/public/script.js
+++ src/Resources/public/script.js
@@ -10,17 +10,26 @@ ContaoFlash.clearMessages = function(){
 
 ContaoFlash.registerEventHandlers = function(){
     var elements = document.querySelectorAll('.mod_flash_messages .flash-message');
+
     if(!elements.length) return;
+
     elements.forEach(function(el){
         var close = el.querySelector('.flash-dismiss');
+
         if(!close) return;
+
         close.addEventListener('click', function(e){
             e.preventDefault();
+
+            let parentChildrenLength = el.parentNode.children.length;
+
             var xhr = new XMLHttpRequest();
             xhr.open('GET', this.href);
             xhr.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
             xhr.send();
             el.parentNode.removeChild(el);
+
+            if(parentChildrenLength === 1) document.querySelectorAll('.mod_flash_messages')[0].remove();
         });
     });
 }
